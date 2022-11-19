b34204359be876e8f3343e5cb429dcb2409b2cf5
Roman Trutnev <104026522+truten-original@users.noreply.github.com>
2022-11-19 22:35:17 +0300

    added routes. nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
-import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import AuthLayout from "./layouts/AuthLayout";
-import PostsLayout from "./layouts/PostsLayout";
-import MainPage from "./pages/MainPage";
-import NavBar from "./components/NavBar/NavBar";
-import ProtectedRoute from "./components/ProtectedRoute";
-import withRedux from "./hoc/withRedux";
-import withRouter from "./hoc/withRouter";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import AuthLayout from './layouts/AuthLayout'
+import PostsLayout from './layouts/PostsLayout'
+import MainPage from './pages/MainPage'
+import NavBar from './components/NavBar/NavBar'
+// import ProtectedRoute from './elements/ProtectedRoute'
+import withRedux from './hoc/withRedux'
+import withRouter from './hoc/withRouter'
+import 'react-toastify/dist/ReactToastify.css'
+import PostsListPage from './pages/Posts/PostsListPage'
+import PostPage from './pages/Posts/PostPage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SigupPage'
 
 function App() {
-    return (
-        <div className='min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-150 flex flex-col'>
-            <NavBar />
-            <Switch>
-                <Route path='/' exact component={MainPage} />
-                <Route path='/auth' component={AuthLayout} />
-                <ProtectedRoute path='/posts/:id?' component={PostsLayout} />
-                <Redirect from='*' to='/' />
-            </Switch>
+  return (
+    <div className="min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-150 flex flex-col">
+      <NavBar />
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path="auth/*" element={<AuthLayout />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route />
+        </Route>
+        <Route path="/posts/*" element={<PostsLayout />}>
+          <Route index element={<PostsListPage />} />
+          <Route path=":postId" element={<PostPage />} />
+        </Route>
+        {/* <Redirect from="*" to="/" /> */}
+      </Routes>
 
-            <ToastContainer />
-        </div>
-    );
+      <ToastContainer />
+    </div>
+  )
 }
-const AppWithStoreAndRoutes = withRedux(withRouter(App));
-export default AppWithStoreAndRoutes;
+const AppWithStoreAndRoutes = withRedux(withRouter(App))
+export default AppWithStoreAndRoutes
--- src/layouts/PostsLayout.jsx
+++ src/layouts/PostsLayout.jsx
@@ -1,39 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react'
 // Librares
-import { useDispatch } from "react-redux";
-import { useRouteMatch, Switch, Route } from "react-router-dom";
-// Components
-import BackgroundGradiend from "../components/BackgroundGradient";
-import ScreenWidthWrapper from "../components/ScreenWidthWrapper";
+import { useDispatch } from 'react-redux'
+import { Outlet } from 'react-router-dom'
+// Elemntelements
+import BackgroundGradiend from '../components/BackgroundGradient'
+import ScreenWidthWrapper from '../components/ScreenWidthWrapper'
 // Pages
-import PostPage from "../pages/Posts/PostPage";
-import PostsListPage from "../pages/Posts/PostsListPage";
 //Store
-import { getPosts } from "../store/postsSlice";
+import { getPosts } from '../store/postsSlice'
 
 const PostsLayout = ({ children }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const loadPostData = () => {
+      dispatch(getPosts())
+    }
+    loadPostData()
+  }, [dispatch])
 
-    useEffect(() => {
-        const loadPostData = () => {
-            dispatch(getPosts());
-        };
-        loadPostData();
-    }, [dispatch]);
+  //   let { path } = useRouteMatch()
+  return (
+    <>
+      <BackgroundGradiend />
+      <ScreenWidthWrapper>
+        <Outlet />
+        {/* <Routes>
+          <Route path={':postId'} element={<PostPage />} />
+          <Route path={''} element={<PostsListPage />} />
+        </Routes> */}
+        {children}
+      </ScreenWidthWrapper>
+    </>
+  )
+}
 
-    let { path } = useRouteMatch();
-    return (
-        <>
-            <BackgroundGradiend />
-            <ScreenWidthWrapper>
-                <Switch>
-                    <Route path={path + "/:postId"} component={PostPage} />
-                    <Route exact path={path} component={PostsListPage} />
-                </Switch>
-                {children}
-            </ScreenWidthWrapper>
-        </>
-    );
-};
-
-export default PostsLayout;
+export default PostsLayout
